8bf578fdb89ee4c513da6c62470e5317a47c93be
hbgames1 <57827813+hbgames1@users.noreply.github.com>
2019-11-16 23:48:23 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("645195007631949838")
+setInterval(function() {
+channel.send(`هلاهلاهلاهلاهلاهلاهلاههلاهلا  كيف الحالكم بيخير وانت افضل يوتيوبر هلا بخير واواواووا`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
